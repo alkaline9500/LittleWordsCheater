ca931ca26942cf1ee315dcf1a5f29a33e663c2ef
alkaline9500 <zimmoz3@verizon.net>
2013-05-08 13:45:07 -0400

    Added git ignore and some implementation

--- LittleWordsCheater.java
+++ LittleWordsCheater.java
@@ -13,24 +13,72 @@ public class LittleWordsCheater
 	private ArrayList<String> stubs;
 	
 	/**
+	 * Constructs a Cheater
+	 */
+	public LittleWordsCheater()
+	{
+		ArrayList<String> stubs = new ArrayList<String>();
+		ArrayList<String> dict = new ArrayList<String>();
+	}
+
+	/**
+	 * Gets all possible permutations from the given stubs
+	 * @param s ArrayList of String stubs
+	 * @return a ArrayList of all permutations
+	 */
+	public ArrayList<String> getPermutations(ArrayList<String> s)
+	{
+		ArrayList<String> words = new ArrayList<String>();
+		if (s.size() > 1)
+		{
+			for (String x : s)
+			{
+				ArrayList<String> temp = new ArrayList<String>(s);
+				temp.remove(x);
+				for (String y : temp)
+				{
+					words.add(x+y);
+				}
+
+			}
+		}
+		return words;
+	}
+
+	/**
+	 * Loads the dictionary from a specific file
+	 * @param filename the file from which to load
+	 */
+	public void loadDict(String filename)
+	{
+		Scanner dictFile = new Scanner(filename);
+		while (dictFile.hasNext())
+		{
+			dict.add(dictFile.next());
+		}
+	}
+	
+
+	/**
 	 * Main Method
 	 * Usage: java LittleWordsCheater dictionary.txt cd {sd re tr ...}
 	 */
 	public static void main(String[] args)
 	{
+		LittleWordsCheater cheat = new LittleWordsCheater();
 		// Must be at least dict and stubs
 		if (args.length < 2)
 		{
 			System.out.println("Usage: java LittleWordsCheater dictionary.txt cd {sd re tr ...}");
+			System.exit(1);
 		}
 
-		// Load the dictionary from the file
-		Scanner dictFile = new Scanner(args[0]);
-		while (dictFile.hasNext())
-		{
-			dict.add(dictFile.next());
-		}
+		cheat.loadDict(args[0]);
 
-
+		ArrayList<String> test = new ArrayList<String>();
+		test.add("th");
+		test.add("st");
+		test.add("er");
+		System.out.println(cheat.getPermutations(test));
 	}
 }
